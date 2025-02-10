eedb23d5605c9d18678daff445c4f310fe39f06b
arakot <primov_98@mail.ru>
2025-02-10 18:48:30 -0500

    added app.py to the repo    "Flask app hello world !"

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__) # create an instance of the Flask class
 @app.route('/') # route() decorator to tell Flask what URL should trigger our function
 
-def home():
+def home(): 
     return "Hello, World!"
 
 if __name__=="__main__":    # If the script is executed directly, the code block will be executed.
