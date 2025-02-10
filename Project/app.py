152e8aa1c58ee04218b9a70c21dc8fb9614215f6
arakot <primov_98@mail.ru>
2025-02-10 18:30:14 -0500

    first app.py hello backend world in flask python

---
+++ Project/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__) # create an instance of the Flask class
+@app.route('/') # route() decorator to tell Flask what URL should trigger our function
+
+def home():
+    return "Hello, World!"
+
+if __name__=="__main__":    # If the script is executed directly, the code block will be executed.
+    app.run(debug=True) # run the application on the local development server
